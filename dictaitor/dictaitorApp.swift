77d144150d12294aee58c358f8986841d536bffd
Bartosz Warzecha <bartosz@warzecha.com>
2026-01-14 17:45:22 -0500

    Initial Commit

---
+++ dictaitor/dictaitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  dictaitorApp.swift
+//  dictaitor
+//
+//  Created by Bartosz on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct dictaitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
